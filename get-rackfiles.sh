0a018ea955600bd1012cd8201874af70d80278cc
reorx <novoreorx@gmail.com>
2017-01-02 14:50:22 -0500

    silent curl

--- get-rackfiles.sh
+++ get-rackfiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/reorx/rackfiles/master/rackfiles.py -o /tmp/rackfiles.py && \
+curl -s https://raw.githubusercontent.com/reorx/rackfiles/master/rackfiles.py -o /tmp/rackfiles.py && \
     chmod +x /tmp/rackfiles.py && \
     sudo mv /tmp/rackfiles.py /usr/local/bin/rackfiles
